fa3a9bd58b826ef9338a905021c2b0db3a62ad9b
Parth Jaggi <parthjaggi28@gmail.com>
2020-12-23 23:10:52 -0500

    minor;

--- src/discretized/Main2.java
+++ src/discretized/Main2.java
@@ -26,7 +26,7 @@ final public class Main2 {
 		int num_iteration = Integer.parseInt(args[1]);
 		_domain = new Domain(domain);
 
-		for (int d = 1000; d <= 1000; d++) {
+		for (int d = 50; d <= 1000; d = d + 50) {
 			System.out.println("");
 			System.out.println("=========================================================");
 			System.out.println("d: " + d);
